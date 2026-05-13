753558052444f8d3bfa9197bbf3ff1f6627c2cc8
Rafael <rafaelkrm@al.insper.edu.br>
2026-05-13 13:40:46 -0300

    add auth shared library

---
+++ src/main/java/store/auth/AuthController.java
@@ -0,0 +1,48 @@
+package store.auth;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import store.account.AccountOut;
+
+@FeignClient(
+    name="auth",
+    url="http://auth:8080"
+)
+public interface AuthController {
+
+    public static String AUTH_COOKIE_TOKEN = "__store_jwt_token";
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<Void> login(
+        @RequestBody LoginIn in
+    );
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<Void> register(
+        @RequestBody RegisterIn in
+    );
+
+    @GetMapping("/auth/whoiam")
+    public ResponseEntity<AccountOut> whoIAm(
+        @RequestHeader(value = "id-account", required = true) String idAccount
+    );
+
+    @GetMapping("/auth/health-check")
+    public ResponseEntity<Void> healthCheck();
+
+    @PostMapping("/auth/solve")
+    public ResponseEntity<Map<String, String>> solveToken(
+        @RequestBody TokenOut map
+    );
+
+    @GetMapping("/auth/logout")
+    public ResponseEntity<Void> logout();
+
+}
